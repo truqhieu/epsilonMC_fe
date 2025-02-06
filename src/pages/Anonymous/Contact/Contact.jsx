f4b2c4cf628697c7a31dadc6f21d995f1782c0b2
NguyenThanhTung <tungnthe161278@fpt.edu.vn>
2025-02-06 11:04:41 +0700

    update

--- src/pages/Anonymous/Contact/Contact.jsx
+++ src/pages/Anonymous/Contact/Contact.jsx
@@ -1,8 +1,44 @@
-// eslint-disable-next-line no-unused-vars
-import React from "react";
+import { assets } from "../../../assets/assets";
+import "./Contact.css";
 
-const Contact = () => {
-  return <div>Contact</div>;
+const ContactPage = () => {
+  return (
+    <div className="contact-page">
+      <div >
+        <div className="contact-content">
+
+          <div className="contact-form">
+
+            <h2>Gửi tin nhắn</h2>
+            <form>
+              <input type="text" placeholder="Họ và tên" required />
+              <input type="email" placeholder="Email" required />
+              <input type="tel" placeholder="Số điện thoại" required />
+              <textarea placeholder="Nội dung tin nhắn" rows="5" required></textarea>
+              <button type="submit">Gửi tin nhắn</button>
+            </form>
+            
+          </div>
+          <div className="contact-info">
+
+            <h2>Thông tin liên hệ</h2>
+            <p><strong>Địa chỉ:</strong>Hoa Lac Hi-tech Park, km 29, Đại lộ Thăng Long, Hà Nội</p>
+            <p><strong>Số điện thoại:</strong> 0123 456 789</p>
+            <p><strong>Email:</strong> trunghieu2003hhh@gmail.com</p>
+
+          </div>
+
+        </div>
+
+        <div className="contact-header">
+
+          <h1>Liên hệ với chúng tôi</h1>
+          <p>Hãy để lại tin nhắn, chúng tôi sẽ phản hồi sớm nhất có thể.</p>
+
+        </div>
+      </div>
+    </div>
+  );
 };
 
-export default Contact;
+export default ContactPage;
