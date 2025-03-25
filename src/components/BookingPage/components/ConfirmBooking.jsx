3f5a54ecc9b558108318f2858b38ccca6b318c44
giahoang2401 <gianhhe161190@fpt.edu.vn>
2025-03-25 21:49:00 +0700

    dev

--- src/components/BookingPage/components/ConfirmBooking.jsx
+++ src/components/BookingPage/components/ConfirmBooking.jsx
@@ -15,7 +15,7 @@ const ConfirmBooking = ({ role }) => {
         <div className="note">
           Tài khoản hệ thống của bạn sẽ sớm được cấp. Hẹn gặp bạn tại buổi hẹn
         </div>
-        <button className="button-confirm" onClick={() => navigate(role ? `/${role}` : "/")}>
+        <button className="button-confirm" onClick={() => navigate(role ? `/${role}` : "")}>
           Trở lại trang chủ
         </button>
       </div>
